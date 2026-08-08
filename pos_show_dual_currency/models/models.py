0ba7548f2e2b8b6511dc9bbe141991a2fc54807e
Juan Cordoba <jgcordobac@gmail.com>
2026-08-08 00:40:17 -0400

    Remove stale POS pricelist constraint override

--- pos_show_dual_currency/models/models.py
+++ pos_show_dual_currency/models/models.py
@@ -1,5 +1,6 @@
-from odoo import fields, models, api, _
-from odoo.exceptions import ValidationError, UserError
+from odoo import fields, models
+
+
 class PosConfig(models.Model):
     _inherit = "pos.config"
 
@@ -24,23 +25,4 @@ class PosConfig(models.Model):
     default_location_src_id = fields.Many2one(
         "stock.location", related="picking_type_id.default_location_src_id"
     )
-
-
-    @api.constrains('pricelist_id', 'use_pricelist', 'available_pricelist_ids', 'journal_id', 'invoice_journal_id', 'payment_method_ids')
-    def _check_currencies(self):
-        for config in self:
-            if config.use_pricelist and config.pricelist_id not in config.available_pricelist_ids:
-                raise ValidationError(_("The default pricelist must be included in the available pricelists."))
-
-            # Check if the config's payment methods are compatible with its currency
-            # for pm in config.payment_method_ids:
-            #     if pm.journal_id and pm.journal_id.currency_id and pm.journal_id.currency_id != config.currency_id:
-            #         raise ValidationError(_("All payment methods must be in the same currency as the Sales Journal or the company currency if that is not set."))
-
-        if any(self.available_pricelist_ids.mapped(lambda pricelist: pricelist.currency_id != self.currency_id)):
-            raise ValidationError(_("All available pricelists must be in the same currency as the company or"
-                                    " as the Sales Journal set on this point of sale if you use"
-                                    " the Accounting application."))
-        if self.invoice_journal_id.currency_id and self.invoice_journal_id.currency_id != self.currency_id:
-            raise ValidationError(_("The invoice journal must be in the same currency as the Sales Journal or the company currency if that is not set."))
 
